5c2925c92093ffc6f88fa43742b6ab6427bc5941
mettal142 <40825944+mettal142@users.noreply.github.com>
2024-03-25 23:46:39 +0900

    Add files via upload

---
+++ Blaster/Pickups/AmmoPickup.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "Blaster/Weapon/WeaponTypes.h"
+#include "AmmoPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AAmmoPickup : public APickup
+{
+	GENERATED_BODY()
+	
+protected :
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+	UPROPERTY(EditAnywhere)
+	int32 AmmoAmount = 30;
+
+	UPROPERTY(EditAnywhere)
+	EWeaponType WeaponType;
+
+
+};
